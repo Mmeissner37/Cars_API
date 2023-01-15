57fbd848b34485d1fbc1963e9a042fd5282069af
Michaela Meissner <m.p.meissner85@gmail.com>
2023-01-15 12:16:00 -0500

    Registered Car Model with Admin site + Completed POST functionality for Car + Updated Rest Framework Decimal Settings

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,7 @@
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import CarSerializer
 from .models import Car
 
@@ -8,17 +9,13 @@ from .models import Car
 @api_view(['GET', 'POST']) #this is for the decorator 
 def cars_list(request):
 
-
     if request.method == 'GET':
         cars = Car.objects.all()
         serializer = CarSerializer(cars, many=True)
         return Response(serializer.data)
 
-
     elif request.method == 'POST':
         serializer = CarSerializer(data=request.data)
-        if serializer.is_valid() == True:
-            serializer.save()
-            return Response(serializer.data, status=201)
-        else:
-            return Response(serializer.errors, status=400)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
